dfdc5293ecdaa6a948953df40c7696a0e9a82280
maxxapok <maxxapok@mail.ru>
2019-03-23 17:49:25 +0300

    first commit

--- browser/slider/js/js.js
+++ browser/slider/js/js.js
@@ -1,12 +1,16 @@
 const imgArr = ["i/airmax-jump.png", "i/airmax-on-foot.png", "i/airmax-playground.png", "i/airmax-top-view.png", "i/airmax.png"];
 console.log(imgArr);
+// let i = 0;
 const sliderImg = document.getElementById('slider');
 sliderImg.src = imgArr[0];
 setTimeout(() => {
   for (let i = 0; i < imgArr.length; ++i) {
+  	console.log(i);
     sliderImg.src = imgArr[i];
   }
-  if (i === imgArr.length - 1) {
-    i = 0;
-  }
-}, 5000);
+  // if (i === imgArr.length - 1) {
+  //   i = 0;
+  // }
+}, 1000);
+
+
