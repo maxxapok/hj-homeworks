4b6f297908ea0736593b348f6600eab6be9c84da
maxxapok <maxxapok@mail.ru>
2019-04-04 22:44:01 +0300

    my latest commit

--- html-element-collection/audioplayer/js/audiopleer.js
+++ html-element-collection/audioplayer/js/audiopleer.js
@@ -2,7 +2,7 @@ const srcArr = ['mp3/LA Chill Tour.mp3', 'mp3/LA Fusion Jam.mp3', 'mp3/This is i
 const titleArr = ['LA Chill Tour', 'This is it band', 'LA Fusion Jam'];
 const mediaplayer = document.getElementsByClassName('mediaplayer')[0];
 const player = mediaplayer.getElementsByTagName('audio')[0];
-// const playstateBtn = document.getElementsByClassName('playstate')[0];
+const playstateBtn = document.getElementsByClassName('playstate')[0];
 const playBtn = document.getElementsByClassName('fa-play')[0];
 const pauseBtn = document.getElementsByClassName('fa-pause')[0];
 const stopBtn = document.getElementsByClassName('stop')[0];
@@ -11,14 +11,13 @@ const nextBtn = document.getElementsByClassName('next')[0];
 const titleSpan = document.getElementsByClassName('title')[0];
 let i = 0;
 
-playBtn.onclick = () => {
-	player.play();
-	mediaplayer.classList.add('play');
-}
-
-pauseBtn.onclick = () => {
-	player.pause();
-	mediaplayer.classList.remove('play');
+playstateBtn.onclick = () => {
+	mediaplayer.classList.toggle('play');
+	if (mediaplayer.classList.contains('play')) {
+		player.play();
+	} else {
+		player.pause();
+	}
 }
 
 stopBtn.onclick = () => {
