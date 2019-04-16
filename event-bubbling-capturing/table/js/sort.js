24cc96260eb3e24e02aa4eab4bd935245f0b4de1
maxxapok <maxxapok@mail.ru>
2019-04-17 00:55:52 +0300

    my latest commit

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,17 +1,15 @@
 'use strict';
-
-const titleCollection = document.querySelectorAll('th');
-// Почему не пригождается? должна
+// const titleCollection = document.querySelectorAll('th');
 let count = 0;
 function handleTableClick(event) {
-  event.currentTarget.dataset.sortBy = event.target.dataset.propName;
+  if (event.target.classList.contains('prop__name')) {
+  event.target.dataset.sortBy = event.target.dataset.propName;
   if (count % 2 === 0) { 
   	event.target.dataset.dir = 1;
   } else {
   	event.target.dataset.dir = -1;
   }
 count++;
-sortTable(event.currentTarget.dataset.sortBy, event.target.dataset.dir);
+sortTable(event.target.dataset.sortBy, event.target.dataset.dir);
+  }
 }
-
-// Работает по клику не только на заголовок, но и любую ячейку столбца, как от этого избавиться?
