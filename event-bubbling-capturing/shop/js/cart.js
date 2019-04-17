cf46c20d56b284382a95e60415f9bf4050e16243
maxxapok <maxxapok@mail.ru>
2019-04-17 21:26:39 +0300

    my latest commit

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,19 @@
 'use strict';
+const showMoreBtn = document.querySelector('.show-more');
+
+function addToCartClick(event) {
+	event.target.title = event.target.dataset.title;
+	event.target.price = event.target.dataset.price;
+	addToCart(event.target);
+}
+
+showMoreBtn.addEventListener('click', getNewList);
+
+ function getNewList(itemsList) {
+ 	const addToCartBtnCollection = document.querySelectorAll('.add-to-cart');
+ 	for (let btn of addToCartBtnCollection) {
+		btn.addEventListener('click', addToCartClick);
+	}	
+}
+
+getNewList();
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,15 +1,14 @@
 'use strict';
-// const titleCollection = document.querySelectorAll('th');
 let count = 0;
 function handleTableClick(event) {
   if (event.target.classList.contains('prop__name')) {
-  event.target.dataset.sortBy = event.target.dataset.propName;
+  // event.target.dataset.sortBy = event.target.dataset.propName;
   if (count % 2 === 0) { 
   	event.target.dataset.dir = 1;
   } else {
   	event.target.dataset.dir = -1;
   }
 count++;
-sortTable(event.target.dataset.sortBy, event.target.dataset.dir);
+sortTable(event.target.dataset.propName, event.target.dataset.dir);
   }
 }
