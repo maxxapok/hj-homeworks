23b4255cc90d9626813d8d9faa8c7ea645a2e7ec
maxxapok <maxxapok@mail.ru>
2019-04-30 23:39:22 +0300

    my latest commit

--- canvas/draw/js/draw.js
+++ canvas/draw/js/draw.js
@@ -1,1 +1,119 @@
 'use strict';
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const BRUSH_RADIUS = 6;
+
+let curves = [];
+let undone = [];
+let drawing = false;
+let weird = false;
+let needsRepaint = false;
+
+window.addEventListener("load", function()
+{
+
+    function draw()
+    {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.moveTo(0, 0); 
+        ctx.lineTo(canvas.width, canvas.height); 
+        ctx.moveTo(canvas.width, 0); 
+        ctx.lineTo(0, canvas.height); 
+        ctx.stroke();
+    }
+    function resize()
+    {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        draw();
+    }
+    window.addEventListener("resize", resize);
+    resize();
+});
+
+
+function circle(point) {
+  ctx.beginPath();
+  ctx.arc(...point, BRUSH_RADIUS / 2, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function smoothCurveBetween (p1, p2) {
+  const cp = p1.map((coord, idx) => (coord + p2[idx]) / 2);
+  ctx.quadraticCurveTo(...p1, ...cp);
+}
+
+function smoothCurve(points) {
+  ctx.beginPath();
+  ctx.lineWidth = BRUSH_RADIUS;
+  ctx.lineJoin = 'round';
+  ctx.lineCap = 'round';
+
+  ctx.moveTo(...points[0]);
+
+  for(let i = 1; i < points.length - 1; i++) {
+    smoothCurveBetween(points[i], points[i + 1]);
+  }
+
+  ctx.stroke();
+}
+
+
+function makePoint(x, y, reflect = false) {
+  return  reflect ? [y, x] : [x, y];
+};
+
+canvas.addEventListener("mousedown", (evt) => {
+  drawing = true;
+  weird = evt.shiftKey; 
+  undone = []; 
+  const curve = []; 
+
+  curve.push(makePoint(evt.offsetX, evt.offsetY, weird)); 
+  curves.push(curve); 
+  needsRepaint = true;
+});
+
+canvas.addEventListener("mouseup", (evt) => {
+  drawing = false;
+});
+
+canvas.addEventListener("mouseleave", (evt) => {
+  drawing = false;
+});
+
+canvas.addEventListener("mousemove", (evt) => {
+  if (drawing) {
+    const point = makePoint(evt.offsetX, evt.offsetY, weird)
+    curves[curves.length - 1].push(point);
+    needsRepaint = true;
+  }
+});
+
+canvas.addEventListener("dblclick", (evt) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+function repaint () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  curves
+    .forEach((curve) => {
+      circle(curve[0]);
+
+      smoothCurve(curve);
+    });
+}
+
+function tick () {
+  if(needsRepaint) {
+    repaint();
+    needsRepaint = false;
+  }
+
+  window.requestAnimationFrame(tick);
+}
+
+tick();
+
---
+++ canvas/star-field/js/star.js
@@ -0,0 +1,27 @@
+'use strict';
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const PI = Math.PI;
+canvas.style.background = 'black';
+const count = Math.random() * (400 - 200) + 200;
+
+function showStars(event) {
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+for (let i = 0; i < count; ++i) {
+ctx.beginPath();
+const radius = Math.random() * (1.1 - 0) + 0;
+const x = Math.random() * (300 - 0) + 0;
+const y = Math.random() * (150 - 0) + 0;
+ctx.globalAlpha = Math.random() * (1 - 0.8) + 0.8;
+const colorsArr = ['#ffffff', '#ffe9c4', '#d4fbff'];
+ctx.fillStyle = colorsArr[Math.floor(Math.random() * colorsArr.length)];
+ctx.arc(x, y, radius, 0, 2 * PI);
+ctx.fill();
+// ctx.closePath();
+  }
+}
+
+canvas.addEventListener('click', showStars);
+// document.addEventListener('DOMContentLoaded', showStars);
+showStars();
+
