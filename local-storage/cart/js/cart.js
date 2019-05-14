21ea4c0d5a4a80b26899a82ce6d7a43bd4c21b68
maxxapok <maxxapok@mail.ru>
2019-05-15 00:50:30 +0300

    my latest commit

--- local-storage/cart/js/cart.js
+++ local-storage/cart/js/cart.js
@@ -1,73 +1,72 @@
-'use strict';
-
+// необходимые блоки и кнопки
 const sizeSwatch = document.getElementById('sizeSwatch'),
-      colorSwatch = document.getElementById('colorSwatch'),
-      cart = document.getElementById('quick-cart'),
-      addToCartForm = document.getElementById('AddToCartForm'),
-      addToCartBtn = document.getElementById('AddToCart');
+    colorSwatch = document.getElementById('colorSwatch'),
+    cart = document.getElementById('quick-cart'),
+    addToCartForm = document.getElementById('AddToCartForm'),
+    addToCartBtn = document.getElementById('AddToCart');
 
-
+// цвета
 fetch('https://neto-api.herokuapp.com/cart/colors')
-  .then(function(response) {
-      return response.json();
-  })
-  .then(colors => {
-    for (const color of colors) {
-      const availability = (color.isAvailable) ? 'available': 'soldout';
-      const disabled = (!color.isAvailable) ? 'disabled' : '';
-      const colorChecked = (localStorage.colorChecked === color.code) ? 'checked' : ''; 
-      colorSwatch.innerHTML += `<div data-value="${color.type}" class="swatch-element color ${color.type} ${availability}">
+    .then(function(response) {
+        return response.json();
+    })
+    .then(colors => {
+        for (const color of colors) {
+            const availability = (color.isAvailable) ? 'available' : 'soldout';
+            const disabled = (!color.isAvailable) ? 'disabled' : '';
+            const colorChecked = (localStorage.colorChecked === color.code) ? 'checked' : '';
+            colorSwatch.innerHTML += `<div data-value="${color.type}" class="swatch-element color ${color.type} ${availability}">
                                   <div class="tooltip">${color.title}</div>
                                   <input quickbeam="color" id="swatch-1-${color.type}" type="radio" name="color" value="${color.type}" ${colorChecked} ${disabled}>  
                                   <label for="swatch-1-${color.type}" style="border-color: ${color.code};">
                                   <span style="background-color: ${color.code};"></span>
                                   <img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
                                   </label>
-                                </div>` 
-    }
-    const inputs = colorSwatch.querySelectorAll('input');
-    for (const input of inputs) {
-       input.addEventListener('click', setCheckedColorStorage); 
-    }
-    
-    function setCheckedColorStorage(event) {      
-      localStorage.colorChecked = event.target.value;      
-    }    
-  });
+                                </div>`
+        }
+        const inputs = colorSwatch.querySelectorAll('input');
+        for (const input of inputs) {
+            input.addEventListener('click', setCheckedColorStorage);
+        }
+        // запоминаем цвет на стороне клиента
+        function setCheckedColorStorage(event) {
+            localStorage.colorChecked = event.target.value;
+        }
+    });
 
-
+// размеры
 fetch('https://neto-api.herokuapp.com/cart/sizes')
-  .then(function(response) {
-      return response.json();
-  })
-  .then(sizes => {
-    for (const size of sizes) {
-      const availability = (size.isAvailable) ? 'available': 'soldout';
-      const disabled = (!size.isAvailable) ? 'disabled' : '';
-      const sizeChecked = (localStorage.sizeChecked === size.type) ? 'checked' : ''; 
-      sizeSwatch.innerHTML += `<div data-value="${size.type}" class="swatch-element plain ${size.type} ${availability}">
+    .then(function(response) {
+        return response.json();
+    })
+    .then(sizes => {
+        for (const size of sizes) {
+            const availability = (size.isAvailable) ? 'available' : 'soldout';
+            const disabled = (!size.isAvailable) ? 'disabled' : '';
+            const sizeChecked = (localStorage.sizeChecked === size.type) ? 'checked' : '';
+            sizeSwatch.innerHTML += `<div data-value="${size.type}" class="swatch-element plain ${size.type} ${availability}">
                                   <input id="swatch-0-${size.type}"type="radio" name="size" value="${size.type}" ${disabled} ${sizeChecked}>
                                   <label for="swatch-0-${size.type}">${size.title}
                                     <img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
                                   </label>
-                                </div>` 
-    }
-    const inputs = sizeSwatch.querySelectorAll('input');
-    for (const input of inputs) {
-       input.addEventListener('click', setCheckedSizeStorage); 
-    }
-    
-    function setCheckedSizeStorage(event) {      
-      localStorage.sizeChecked = event.target.value;      
-    }    
-  });
+                                </div>`
+        }
+        const inputs = sizeSwatch.querySelectorAll('input');
+        for (const input of inputs) {
+            input.addEventListener('click', setCheckedSizeStorage);
+        }
+        // запоминаем размер на стороне клиента
+        function setCheckedSizeStorage(event) {
+            localStorage.sizeChecked = event.target.value;
+        }
+    });
 
-
+// функция обновления корзины на странице
 function updateCart(products) {
-  cart.innerHTML = '';
-  let totalPrise = 0;
-  for (const product of products) { 
-     cart.innerHTML += `<div class="quick-cart-product quick-cart-product-static" id="quick-cart-product-${product.id}" style="opacity: 1;">
+    cart.innerHTML = '';
+    let totalPrise = 0;
+    for (const product of products) {
+        cart.innerHTML += `<div class="quick-cart-product quick-cart-product-static" id="quick-cart-product-${product.id}" style="opacity: 1;">
                           <div class="quick-cart-product-wrap">
                             <img src="${product.pic}" title="${product.title}">
                             <span class="s1" style="background-color: #000; opacity: .5">${product.price}</span>
@@ -75,83 +74,83 @@ function updateCart(products) {
                           </div>
                           <span class="count hide fadeUp" id="quick-cart-product-count-${product.id}">${product.quantity}</span>
                           <span class="quick-cart-product-remove remove" data-id="${product.id}"></span>
-                        </div>`; 
-      totalPrise += product.price*product.quantity;      
-    }  
-    
+                        </div>`;
+        totalPrise += product.price * product.quantity;
+    }
+
     cart.innerHTML += `<a id="quick-cart-pay" quickbeam="cart-pay" class="cart-ico open">
                           <span><strong class="quick-cart-text">Оформить заказ<br></strong>
                             <span id="quick-cart-price">${totalPrise}</span>
                           </span>
-                       </a>`; 
+                       </a>`;
     const quickCartPay = cart.querySelector('#quick-cart-pay');
     products.length ? quickCartPay.classList.add('open') : quickCartPay.classList.remove('open');
-    
+
     const btnsRemove = cart.getElementsByClassName('remove');
-    for (const btnRemove of btnsRemove) {      
-      btnRemove.addEventListener('click', removeFromCart);  
-    }   
+    for (const btnRemove of btnsRemove) {
+        btnRemove.addEventListener('click', removeFromCart);
+    }
 }
 
-
+// корзина
 fetch('https://neto-api.herokuapp.com/cart')
-  .then(function(response) {
-      return response.json();
-  })
-  .then(products => {
-    updateCart(products);  
-  });
+    .then(function(response) {
+        return response.json();
+    })
+    .then(products => {
+        updateCart(products);
+    });
 
-
+// функция отправки товара в корзину
 function sendAddToCartForm(event) {
-  event.preventDefault();
-  
-  const formData = new FormData(addToCartForm);
+    event.preventDefault();
+    // получаем данные формы
+    const formData = new FormData(addToCartForm);
 
-  
-  formData.append("productId", addToCartForm.dataset.productId);
-  
-  const sendOptions = {
-    method: 'POST',
-    body: formData
-  }  
-  
-  fetch('https://neto-api.herokuapp.com/cart', sendOptions)
-  .then(function(response) {
-      return response.json();
-  })
-  .then(products => {
-    updateCart(products);    
-  })
-  .catch(function(error) {
-    console.log('Что-то пошло не так');
-  });  
+    // добавляем к ней id товара
+    formData.append("productId", addToCartForm.dataset.productId);
+
+    const sendOptions = {
+        method: 'POST',
+        body: formData
+    }
+
+    fetch('https://neto-api.herokuapp.com/cart', sendOptions)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(products => {
+            updateCart(products);
+        })
+        .catch(function(error) {
+            console.log('Что-то пошло не так');
+        });
 }
 
 addToCartBtn.addEventListener('click', sendAddToCartForm);
 
-
+// удаление из корзины
 function removeFromCart() {
-  
-  const formData = new FormData();  
-  formData.append("productId", this.dataset.id);
-  
-  const sendOptions = {
-    method: 'POST',
-    body: formData
-  }  
-  
-  fetch('https://neto-api.herokuapp.com/cart/remove', sendOptions)
-  .then(function(response) {
-      return response.json();
-  })
-  .then(products => {
-    if (products.hasOwnProperty('error')) {
-      alert('Произошла ошибка: ' + products.message);    
+    // добавляем для отправки id товара
+    const formData = new FormData();
+    formData.append("productId", this.dataset.id);
+
+    const sendOptions = {
+        method: 'POST',
+        body: formData
     }
-    updateCart(products);    
-  })
-  .catch(function(error) {
-    alert('Что-то пошло не так');
-  });       
+
+    fetch('https://neto-api.herokuapp.com/cart/remove', sendOptions)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(products => {
+            if (products.hasOwnProperty('error')) {
+                alert('Произошла ошибка: ' + products.message);
+            }
+            updateCart(products);
+        })
+        .catch(function(error) {
+            alert('Что-то пошло не так');
+        });
 }
--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,46 +1,49 @@
+'use strict';
+// записывам формы и пути в переменные
 const formLogin = document.querySelector('.sign-in-htm'),
-      formRegistration  = document.querySelector('.sign-up-htm'),
-      pathLogin = 'https://neto-api.herokuapp.com/signin',
-      pathRegistration = 'https://neto-api.herokuapp.com/signup';
-
+    formRegistration = document.querySelector('.sign-up-htm'),
+    pathLogin = 'https://neto-api.herokuapp.com/signin',
+    pathRegistration = 'https://neto-api.herokuapp.com/signup';
+// функция активации работы форм
 function activatesSendForms(form, path) {
-  
-  const btnSend = form.querySelector('input[type="submit"]');
-  const output = form.querySelector('output');
-  
-  const successfulMessage = form.classList.contains('sign-in-htm') ? 'авторизован': 'зарегистрирован';
-  
-  btnSend.addEventListener('click', sendData);
-  function sendData(event) {
-    event.preventDefault();
-    
-    const data = {};
-    const inputs = form.querySelectorAll('.input');
-    for (let input of inputs) {
-       let name = input.name;
-       let value = input.value;
-       data[name] = value;
-    }    
-    
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', onLoad);
-    xhr.open('POST', path);
-    xhr.setRequestHeader('Content-Type','application/json');
-    xhr.send(JSON.stringify(data));  
-    function onLoad() {
-      const response = JSON.parse(xhr.response);
-      
-      if ('error' in response) {        
-        output.innerText = response.message;
-      } else {
-        output.innerText = `Пользователь ${response.name} успешно ${successfulMessage} `;
-      }      
-    }
-  }
-}
+    // находим кнопку и поле вывода
+    const btnSend = form.querySelector('input[type="submit"]');
+    const output = form.querySelector('output');
+    // определяем окончание фразы в случае успешного исхода
+    const successfulMessage = form.classList.contains('sign-in-htm') ? 'авторизован' : 'зарегистрирован';
+    // навешиваем на кнопку обработчик
+    btnSend.addEventListener('click', sendData);
 
+    function sendData(event) {
+        event.preventDefault();
+        // отправляем данные на сервер в формате json
+        const data = {};
+        const inputs = form.querySelectorAll('.input');
+        for (let input of inputs) {
+            let name = input.name;
+            let value = input.value;
+            data[name] = value;
+        }
+
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', onLoad);
+        xhr.open('POST', path);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(data));
+
+        function onLoad() {
+            // после получения ответа определяем, ошибка ли это 
+            // и результат выводим в соответствующее поле
+            const response = JSON.parse(xhr.response);
+
+            if ('error' in response) {
+                output.innerText = response.message;
+            } else {
+                output.innerText = `Пользователь ${response.name} успешно ${successfulMessage} `;
+            }
+        }
+    }
+}
+// активируем формы
 activatesSendForms(formLogin, pathLogin);
 activatesSendForms(formRegistration, pathRegistration);
-
-
-
